fb1c485970def2a764fb0fe8e47b9f0aa4d8b2b9
SABLAYROLLES <louis.sablayrolles@gmail.com>
2018-04-06 15:38:26 +0200

    add comment and rename vars of extract_features.py

--- extract_features.py
+++ extract_features.py
@@ -1,9 +1,11 @@
 import librosa
 
 def get_mfcc(file):
-	y, sr = librosa.load(file)
-	return librosa.feature.melspectrogram(y=y, sr=sr)
+	#return the mfcc array of an audio file
+	data, soundrate = librosa.load(file)
+	return librosa.feature.melspectrogram(y=data, sr=soundrate)
 	
 def get_loudness(file):
-	y, sr = librosa.load(file)
-	return librosa.feature.rmse(y=y)
+	#return the loudness array of an audio file
+	data, soundrate = librosa.load(file)
+	return librosa.feature.rmse(y=data)
